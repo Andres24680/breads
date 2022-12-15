e0fa2da527daccbbfc28bd74e44c4b5211a550ad
Andres24680 <tasboyandresrestrepo@aol.com>
2022-12-15 18:48:31 -0500

    setting up my port

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+//Routes 
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads!')
+})
+
+//Listen 
+app.listen(PORT, () => {
+    console.log('listening on port', PORT)
+})
