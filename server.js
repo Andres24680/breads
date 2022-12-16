93ab48586dbd6eacb0c76f61b9ddafb843aad9fe
Andres24680 <tasboyandresrestrepo@aol.com>
2022-12-15 19:20:02 -0500

    added

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-
-// DEPENDENCIES
+const { application } = require('express')
 const express = require('express')
 
-// CONFIGURATION
+//config 
 require('dotenv').config()
-const PORT = process.env.PORT
-const app = express()
+const PORT = process.env.PORT 
+console.log(PORT)
 
-// ROUTES
-app.get('/', (req, res) => {
-  res.send('Welcome to an Awesome App about Breads!')
+//routes 
+application.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads!')
 })
 
-// Breads
-const breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController)
+//Breads
+const breadsController = require('./controllers/breads_controllers.js')
+application.use('/breads, breadsController')
 
-// LISTEN
-app.listen(PORT, () => {
-  console.log('listening on port', PORT);
+//listen 
+application.listen(PORT, () => {
+    console.log('listening on port, PORT')
 })
