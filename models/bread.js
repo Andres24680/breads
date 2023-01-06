b1555b99161d124f719ee255218c1f7c938e8a2d
Andres24680 <tasboyandresrestrepo@aol.com>
2023-01-05 20:54:32 -0500

    adding bakedBy to show in console

--- models/bread.js
+++ models/bread.js
@@ -14,5 +14,10 @@ const breadSchema = new Schema({
   }
 })
 
+//helper instance method 
+breadSchema.methods.getBakedBy = function() {
+  return `${this.name} was baked with love by ${this.baker}`
+}
+
 const Bread = mongoose.model('Bread', breadSchema)
 module.exports = Bread 
