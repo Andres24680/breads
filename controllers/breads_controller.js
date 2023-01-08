692654d5bcc853f32c16c67414f251fb8240cacb
Andres24680 <tasboyandresrestrepo@aol.com>
2023-01-08 11:20:59 -0500

    adding .populate method to show tag line

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -110,6 +110,7 @@ breads.get('/:id/edit', (req, res) => {
 // SHOW
 breads.get('/:id', (req, res) => {
   Bread.findById(req.params.id)
+      .populate('baker')
       .then(foundBread => {
          const bakedBy = foundBread.getBakedBy()
          console.log(bakedBy)
